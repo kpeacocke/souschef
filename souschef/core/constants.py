b827fc6d2ae6c910b092911104ab95aad80960ba
Kristian Peacocke <krpeacocke@gmail.com>
2026-01-06 23:21:56 +0000

    refactor: extract resource converter module
    - Updated constants.py with RESOURCE_MAPPINGS and ACTION_TO_STATE
    - Created souschef/converters/resource.py with convert_resource_to_task
    - Handles conversion of Chef resources to Ansible tasks
    - Extracts ~230 lines from server.py

--- souschef/core/constants.py
+++ souschef/core/constants.py
@@ -46,3 +46,39 @@ INSPEC_SHOULD_EXIST = "    it { should exist }"
 ERROR_FILE_NOT_FOUND = "Error: File not found at {path}"
 ERROR_IS_DIRECTORY = "Error: {path} is a directory, not a file"
 ERROR_PERMISSION_DENIED = "Error: Permission denied for {path}"
+
+# Chef resource to Ansible module mappings
+RESOURCE_MAPPINGS = {
+    "package": "ansible.builtin.package",
+    "apt_package": "ansible.builtin.apt",
+    "yum_package": "ansible.builtin.yum",
+    "service": ANSIBLE_SERVICE_MODULE,
+    "systemd_unit": "ansible.builtin.systemd",
+    "template": "ansible.builtin.template",
+    "file": "ansible.builtin.file",
+    "directory": "ansible.builtin.file",
+    "execute": "ansible.builtin.command",
+    "bash": "ansible.builtin.shell",
+    "script": "ansible.builtin.script",
+    "user": "ansible.builtin.user",
+    "group": "ansible.builtin.group",
+    "cron": "ansible.builtin.cron",
+    "mount": "ansible.builtin.mount",
+    "git": "ansible.builtin.git",
+}
+
+# Chef action to Ansible state mappings
+ACTION_TO_STATE = {
+    "create": "present",
+    "delete": "absent",
+    "remove": "absent",
+    "install": "present",
+    "upgrade": "latest",
+    "purge": "absent",
+    "enable": "started",
+    "disable": "stopped",
+    "start": "started",
+    "stop": "stopped",
+    "restart": "restarted",
+    "reload": "reloaded",
+}
